e06de412adbe2b37f88d6a299d84ed6594a45ca3
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2025-01-07 17:52:35 +0800

    Array Problem > 2. Unique integers in array

--- LeetCodeSolutions/ArraySolutions.cs
+++ LeetCodeSolutions/ArraySolutions.cs
@@ -33,4 +33,51 @@ public class ArraySolutions {
         //return empty array if no solution is found
         return new int[]{}; 
     }
+
+    /// <summary>
+    /// Remove Duplicates from Sorted Array
+    /// </summary>
+    /// <param name="ints"></param>
+    /// <returns></returns>
+    public int[] RemoveIntegerDuplicatesInSortedArray(int[] ints){
+        //Edge case: if the array is empty, return an empty array
+        if(ints.Length == 0) return new int[]{};
+        //create a new array to store the unique numbers
+        //initialize the first element of the new array to the first element of the input array
+        var returnValue = new int[1]{ ints[0] };
+        //initialize a variable to keep track of the index of the unique numbers
+        int index = 1;
+        //iterate through the array
+        for(int i=1; i < ints.Length; i++){
+            if(ints[i] != ints[i-1]){
+                //Resize Array
+                Array.Resize(ref returnValue, index + 1);
+                //if the current number is not equal to the next number, add the current number to the new array
+                returnValue[index] = ints[i];
+                //increment the index
+                index++;
+            }
+        }
+        return returnValue;
+    }
+
+    /// <summary>
+    /// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int RemoveArrayDuplicates(int[] nums)
+    {
+        if(nums.Length == 0) return 0;
+        int index = 0;
+        for(int j = 1; j < nums.Length; j++)
+        {
+            if(nums[j] != nums[index])
+            {
+                index++;
+                nums[index] = nums[j];
+            }
+        }
+        return index + 1;
+    }
 }
--- LeetCodeTests/ArrayTests.cs
+++ LeetCodeTests/ArrayTests.cs
@@ -18,4 +18,36 @@ public class ArrayTests
         //Assert
         Assert.Equal(expected, result);
     }
+
+    [Theory]
+    [InlineData(new int[]{}, new int[]{})]
+    [InlineData(new int[] { 1, 1, 2 }, new int[] { 1, 2 })]
+    [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, new int[] { 0, 1, 2, 3, 4 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1 })]
+    public void RemoveIntegerDuplicatesInSortedArray_Test(int[] nums, int[] expected)
+    {
+        //Arrange 
+        var sut = new ArraySolutions();
+        //Act
+        var result = sut.RemoveIntegerDuplicatesInSortedArray(nums);
+        //Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(new int[]{}, 0)]
+    [InlineData(new int[] { 1, 1, 2 }, 2)]
+    [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 }, 5)]
+    [InlineData(new int[] { 1, 1, 1, 1, 1 }, 1)]
+    public void RemoveArrayDuplicates_ReturnLength_Test(int[] nums, int acceptedLength)
+    {
+        //Arrange 
+        var sut = new ArraySolutions();
+        //Act
+        var result = sut.RemoveArrayDuplicates(nums);
+        //Assert
+        Assert.Equal(acceptedLength, result);
+    }
 }
